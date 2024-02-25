3651a21f2b2897c77c82abd1a2b00ca344bc6409
Aber Islam <71089269+iamaber@users.noreply.github.com>
2024-02-25 11:57:51 +0600

    Created The Post Model
    wrote migration code in db/migrate/{timestamp}_create_posts.rb

---
+++ db/migrate/20240225054900_create_posts.rb
@@ -0,0 +1,14 @@
+    # db/migrate/{timestamp}_create_posts.rb
+    
+    class CreatePosts < ActiveRecord::Migration[5.2]
+      def change
+        create_table :posts do |t|
+          t.text :title
+          t.text :body
+          t.text :author
+          t.text :media
+    
+          t.timestamps
+        end
+      end
+    end
